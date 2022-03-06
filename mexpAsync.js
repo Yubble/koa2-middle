1b8aa016851e3126675536bcad3877d900e81f85
liuyanbao <liuyanbao@shuidi-inc.com>
2022-03-06 12:55:45 +0800

    feat: 更新代码

--- mexpAsync.js
+++ mexpAsync.js
@@ -7,16 +7,18 @@
 const express = require('express')
 const app = express()
 
-app.use((res, req, next) => {
+app.use((req, res, next) => {
   console.log('第一层 开始')
+  res.send('响应中间')
   next()
   console.log('第一层 结束')
 })
 
-app.use((res, req, next) => {
+app.use((req, res, next) => {
   console.log('第二层 开始')
   next()
+  res.send('响应最后')
   console.log('第二层 结束')
 })
 
-app.listen(3100)
+app.listen(3300)
--- mkoa.js
+++ mkoa.js
@@ -1,8 +1,24 @@
+/**
+ * @Name: 
+ * @Description: 
+ * @Author: 刘燕保
+ * @Date: 2021-12-23 23:14:21
+ **/
 const koa = require('koa2')
 const app = new koa()
 
-app.use(async ctx => {
-  ctx.body = 'hi koa2'
+app.use(async (ctx, next) => {
+  console.log('第一层 开始')
+  ctx.body = { data: '相应中间' }
+  await next()
+  console.log('第一层 结束')
+})
+
+app.use(async (ctx, next) => {
+  console.log('第二层 开始')
+  await next()
+  ctx.body = { data: '相应最后' }
+  console.log('第二层 结束')
 })
 
 app.listen(3200)
